41f77f1c29411a04d3c0597d7fe1e0f26e253152
shathaway <shathawa@e-z.net>
2021-01-09 12:08:12 -0800

    Move mytest files to their own subdirectory.

---
+++ mytest/mytest.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var getSettings = require('../index.js').getProgramSettings;
+
+(async function () {
+
+  let options = {
+    'default': {
+      'Default1': 'Default Value 1'
+    },
+    'file': 'settings-file.json',
+    'argenv': 'SETTINGS'
+  };
+
+//  var theSettings = await 
+
+  getSettings(options).then(function (theSettings) {
+
+  if (typeof theSettings === 'object') console.log('TEST OF PROGRAM SETTINGS = \n' +
+    JSON.stringify(theSettings,null,2));
+
+  console.log('END OF TEST');
+  });
+}());
+
+
+
+
